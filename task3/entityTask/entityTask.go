2e2d6045c189bac812409d0ff95ca6fd51a22540
YoungStrive <ylstrive@163.com>
2025-09-07 22:02:35 +0800

    2025-09-07 ylstrive gorm 代码提交

--- task3/entityTask/entityTask.go
+++ task3/entityTask/entityTask.go
@@ -1,6 +1,7 @@
 package entityTask
 
 import (
+	"fmt"
 	"gorm.io/gorm"
 	"time"
 )
@@ -42,6 +43,31 @@ type PostComment struct {
 }
 
 func Run(db *gorm.DB) {
-	//需要用指针
-	db.Debug().AutoMigrate(&User{}, &Post{}, &PostComment{})
+	//需要用指针 创建表
+	//db.Debug().AutoMigrate(&User{}, &Post{}, &PostComment{})
+
+	//保存数据
+	user := User{
+		Name: "LiSi", Age: 20, Birthday: time.Now(),
+		PostList: []Post{{Title: "标题1", Content: "如何上岸",
+			CommentList: []PostComment{{Comment: "要上岸,先修路"},
+				{Comment: "马无野草不肥"}}},
+		},
+	}
+
+	user1 := User{
+		Name: "ZhanSan", Age: 18, Birthday: time.Now(),
+		PostList: []Post{{Title: "我是张三", Content: "张三的修行之路",
+			CommentList: []PostComment{{Comment: "事虽难，做则可成！"}}},
+		},
+	}
+	users := []*User{&user, &user1}
+	fmt.Println("users:", users)
+	//保存数据
+	//db.Debug().Create(users)
+	var findUser User
+	findUser.ID = 1
+	//预加载
+	db.Debug().Model(&findUser).Association("PostList").Find(&findUser.PostList)
+
 }
