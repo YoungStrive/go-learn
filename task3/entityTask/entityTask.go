2702f4d4832555df60e2b533d8dbb985055583c3
YoungStrive <ylstrive@163.com>
2025-09-08 21:36:39 +0800

    2025-09-08 ylstrive gorm  作业 代码提交

--- task3/entityTask/entityTask.go
+++ task3/entityTask/entityTask.go
@@ -13,6 +13,8 @@ type User struct {
 	Age      uint8
 	Birthday time.Time
 	gorm.Model
+	//文章统计数
+	PostCount uint8
 	//一个用户可以写很多文章一对多
 	PostList []Post `gorm:"foreignKey:RefUserID"`
 }
@@ -42,32 +44,73 @@ type PostComment struct {
 	gorm.Model
 }
 
+// AfterCreate 钩子函数创建文章之后
+func (p *Post) AfterCreate(tx *gorm.DB) (err error) {
+	postCount, ok := tx.InstanceGet("postCount")
+	postCountNum := postCount.(int64)
+	//成功
+	if ok {
+		fmt.Printf("postCount==%d", postCountNum)
+		postCountNum++
+		tx.Model(&User{}).Where(&User{ID: p.RefUserID}).Update("PostCount", postCountNum)
+	}
+
+	return
+}
+
 func Run(db *gorm.DB) {
 	//需要用指针 创建表
 	//db.Debug().AutoMigrate(&User{}, &Post{}, &PostComment{})
 
 	//保存数据
-	user := User{
-		Name: "LiSi", Age: 20, Birthday: time.Now(),
-		PostList: []Post{{Title: "标题1", Content: "如何上岸",
-			CommentList: []PostComment{{Comment: "要上岸,先修路"},
-				{Comment: "马无野草不肥"}}},
-		},
-	}
-
-	user1 := User{
-		Name: "ZhanSan", Age: 18, Birthday: time.Now(),
-		PostList: []Post{{Title: "我是张三", Content: "张三的修行之路",
-			CommentList: []PostComment{{Comment: "事虽难，做则可成！"}}},
-		},
-	}
-	users := []*User{&user, &user1}
-	fmt.Println("users:", users)
+	//user := User{
+	//	Name: "LiSi", Age: 20, Birthday: time.Now(),
+	//	PostList: []Post{{Title: "标题1", Content: "如何上岸",
+	//		CommentList: []PostComment{{Comment: "要上岸,先修路"},
+	//			{Comment: "马无野草不肥"}}},
+	//	},
+	//}
+	//
+	//user1 := User{
+	//	Name: "ZhanSan", Age: 18, Birthday: time.Now(),
+	//	PostList: []Post{{Title: "我是张三", Content: "张三的修行之路",
+	//		CommentList: []PostComment{{Comment: "事虽难，做则可成！"}}},
+	//	},
+	//}
+	//users := []*User{&user, &user1}
 	//保存数据
 	//db.Debug().Create(users)
 	var findUser User
 	findUser.ID = 1
-	//预加载
-	db.Debug().Model(&findUser).Association("PostList").Find(&findUser.PostList)
+	////查询用户id 为1发布的所有文章及其对应的评论信息。
+	//db.Debug().Model(&findUser).Preload("CommentList").Association("PostList").
+	//	Find(&findUser.PostList)
+	//var count int64
+	//
+	//db.Debug().Model(&PostComment{}).
+	//	Group("PostId").Count(&count)
+	//	var resultPost Post
+	//查询评论数量最多的文章信息。
+	//db.Table("posts").
+	//	Select("posts.*").
+	//	Joins("inner join (select post_id, count(*) as commentCount from post_comments " +
+	//		"group by post_id order by commentCount desc limit 1) as sub on posts.id = sub.post_id").
+	//	Scan(&resultPost)
+	//fmt.Print(resultPost.Title)
+
+	var postCount int64
+	//查询用户id 为1的 文章数
+	db.Debug().Model(&Post{}).Where(&Post{RefUserID: findUser.ID}).Count(&postCount)
+	fmt.Printf("保存之前的用户的文章数，%d", postCount)
+	addPost := Post{Title: "我是新加的哦", Content: "内容暂时保密", RefUserID: findUser.ID}
+
+	//传一个参数过去
+	db.Debug().InstanceSet("postCount", postCount).Create(&addPost)
+	var postCountAfter int64
+
+	db.Debug().Model(&User{}).Select("post_count").Where(&User{ID: findUser.ID}).
+		Find(&postCountAfter)
+
+	fmt.Printf("保存之后的用户的文章数，%d", postCountAfter)
 
 }
